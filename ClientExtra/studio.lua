42af2232737ff368a7c8d0f54b85cee1b74401b1
Nat <95653800+natcat256@users.noreply.github.com>
2023-11-06 03:51:40 -0500

    studio.lua

---
+++ ClientExtra/studio.lua
@@ -0,0 +1,12 @@
+local InsertService = game:GetService("InsertService")
+InsertService:SetTrustLevel(0)
+InsertService:SetBaseSetsUrl("http://www.roblox.com/Game/Tools/InsertAsset.ashx?nsets=10&type=base")
+InsertService:SetUserSetsUrl("http://www.roblox.com/Game/Tools/InsertAsset.ashx?nsets=20&type=user&userid=%d")
+InsertService:SetCollectionUrl("http://www.roblox.com/Game/Tools/InsertAsset.ashx?sid=%d")
+InsertService:SetAssetUrl("https://assetdelivery.roblox.com/v1/asset/?id=%d")
+InsertService:SetAssetVersionUrl("https://assetdelivery.roblox.com/v1/asset/?assetversionid=%d")
+InsertService:SetFreeModelUrl("http://www.roblox.com/Game/Tools/InsertAsset.ashx?type=fm&q=%s&pg=%d&rs=%d")
+InsertService:SetFreeDecalUrl("http://www.roblox.com/Game/Tools/InsertAsset.ashx?type=fd&q=%s&pg=%d&rs=%d")
+
+local ScriptContext = game:GetService("ScriptContext")
+AddLocalStarterScript("StarterScript", ScriptContext)
